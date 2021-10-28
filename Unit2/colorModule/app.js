52f81dc206ccd7cd95175c5eab9392c7c80fb9b2
Luis <dojas64@gmail.com>
2021-10-28 08:16:08 -0500

    chore: arrowFunction done

--- Unit2/colorModule/app.js
+++ Unit2/colorModule/app.js
@@ -8,8 +8,23 @@ const initialMessage =  chalk.blue("App running...");
 console.log( initialMessage );
 
 
+//An object:
+const device = {
+        sensorModel: "HC04",
+        count: 0,
+        date: new Date(),
+};
 
 //Crear un archivo en la base de datos local!
-fs.writeFileSync("./db/dataInfomation.txt", "Server Information");
+fs.writeFileSync("./db/log.json", '');
 console.log( chalk.green("dataInfomation was created successfully!") );
+
+try{
+        fs.appendFileSync("/db/log.json", JSON.stringify(device));
+        console.log(chalk.green("JSON done!"));
+} catch(error) {
+        console.log(chalk.red( chalk.red(`Error: ${error}`)));
+}
+console.log(chalk.yellow("Algoritmos running...."));
+
 
